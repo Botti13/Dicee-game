555f002e4a6c98715ecfa0fcd05b42328e7e52c9
Angel Botti <117987055+Botti13@users.noreply.github.com>
2023-02-07 05:10:28 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function checkRefresh() {
+    if (!sessionStorage.getItem("rollDice")) {
+      sessionStorage.setItem("rollDice", "extra")
+   
+    } else {
+      rollTheDice()
+    }  
+  }
+
+function rollTheDice() {
+
+        const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        const randomImageSource = "images/dice" + randomNumber1 + ".png";
+        const randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+        document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+        document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+        if (randomNumber1 > randomNumber2) {
+            document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+        } else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+        } else {
+            document.querySelector("h1").innerHTML = "Draw";  
+        }
+  }
+
+  document.querySelector("body").onload = checkRefresh();  
